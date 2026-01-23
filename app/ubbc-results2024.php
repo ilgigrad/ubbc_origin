0e46b04601d65abd285a98300fcd9bcd2885e19f
David Mathieu <dmathieu@amarena.io>
2026-01-23 23:05:45 +0100

    refactor results 2024

--- app/ubbc-results2024.php
+++ app/ubbc-results2024.php
@@ -8,23 +8,26 @@ elseif (isset($_GET['init']) && $_GET['init']==1){
     init_live();
 }
 
+// Valeurs par défaut
+$nbpage = (int)($_GET['nbpage'] ?? 1);
+if ($nbpage < 1) $nbpage = 1;
+
+// $races doit être un tableau
+$races = $races ?? [];
+if (!is_array($races)) $races = [];
+
 
 
 $link = connect();
 
 
-if (isset($_GET['page']) && $_GET['page']>0 && $_GET['page']<=$nbpage){
-        $cpage=$_GET['page'];
-}
-else {
-    $cpage=1;
-}
 
-if (isset($_GET['order']) && in_array($_GET['order'],array('bib','lastname','gender','category','srank','rrank','crank','faster','duration','laps','expected','current','pending','race'))){
+
+if (isset($_GET['order']) && in_array($_GET['order'],array('bib','lastname','gender','category','srank','rrank','crank','faster','duration','laps','chrono','average','race'))){
     $order=$_GET['order'];
 }
 else {
-    $order='srank';
+    $order='race desc, rrank';
 }
 
 if (isset($_GET['page']) && isset($_GET['asc']) && in_array($_GET['asc'],array('asc','desc'))){
@@ -37,20 +40,18 @@ else {
     $asc='asc';
 }
 
-if (isset($_GET['race']) && array_key_exists($_GET['race'],$races)){
-    $race=$_GET['race'];
-    $raceFilter="WHERE race = $race ";
-    //$raceFilter="WHERE race ='".$races[$race]."' ";
-}
-else {
-    $race=0;
-    $raceFilter='';
+$race = isset($_GET['race']) ? (int)$_GET['race'] : 0;
+
+if ($race > 0) {
+    $raceFilter = "WHERE race = $race ";
+} else {
+    $raceFilter = '';
 }
 if ($order=="grank"){
-  $order='gender asc, grank';
+    $order='gender asc, grank';
 }
 if ($order=="crank"){
-  $order='gender asc, crank';
+    $order='gender asc, crank';
 }
 
 $sqlcount = 'SELECT count(bib) as nb FROM results_2024 '.$raceFilter.' ORDER BY bib ASC ';
@@ -62,141 +63,128 @@ $nbperpage=25;
 $nbpage=ceil($nbusers/$nbperpage);
 
 if (isset($_GET['page']) && $_GET['page']>0 && $_GET['page']<=$nbpage){
-        $cpage=$_GET['page'];
+    $cpage=$_GET['page'];
 }
 else {
     $cpage=1;
 }
 
 ?>
-  <?php include("ubbc-header.html"); ?>
-  <section class="container-fluid">
-        <div class="row flex-column">
+<?php include("ubbc-header.html"); ?>
+<section class="container-fluid">
+    <div class="row flex-column">
         <!--<div class="panel-sub">    -->
-            <h1 class="fl-txt-gray fl-txt-25 text-uppercase pt-2 text-center">RESULTATS UBBC 9 - 2024</h1>
-            <div class="row p-1 mx-0 mx-md-auto ">
+        <h1 class="fl-txt-gray fl-txt-25 text-uppercase pt-2 text-center">RESULTATS UBBC 10 - 2024</h1>
+        <div class="row p-1 mx-0 mx-md-auto ">
             <!--  <p class="fl-txt-prune fl-txt-30 text-center"> <?php if ($nbpage>1) {echo 'page : '.$cpage;} ?></p>-->
-            </div>
-            <table class="mx-auto table table-stripped table-hover table-bordered table-sm vw-100 table-responsive-lg">
-                <thead class="thead-light fl-bg-apricot fl-txt-prune fl-txt-hov-sadsea">
-                <tr>
-                    <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=srank&asc=<?php echo $asc ?>'>Rang</a></th>
-                    <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=laps&asc=<?php echo $asc ?>'>Tours</a></th>
-                    <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=bib&asc=<?php echo $asc ?>'>BIB</a></th>
-                    <th class="large"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=lastname&asc=<?php echo $asc ?>'>Nom</a></th>
-                    <th class="large"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=firstname&asc=<?php echo $asc ?>'>Pr&eacute;nom</a></th>
-                    <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=gender&asc=<?php echo $asc ?>'>Genre</a></th>
-                    <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=category&asc=<?php echo $asc ?>'>Cat&eacute;gorie</a></th>
-                    <th class="medium"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=faster&asc=<?php echo $asc ?>'>Record</a></th>
-                    <th class="medium"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=duration&asc=<?php echo $asc ?>'>Chrono</a></th>
-                    <th class="medium"><a  href='ubbc-results2024.php?race=<?php echo $race ?>&order=race&asc=<?php echo $asc ?>'>Course</a></th>
-                    <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=rrank&asc=<?php echo $asc ?>'>Rang Course</a></th>
-                    <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=crank&asc=<?php echo $asc ?>'>Rang Genre</a></th>
-                </tr>
-                </thead>
-<?php
+        </div>
+        <table class="mx-auto table table-stripped table-hover table-bordered table-sm vw-100 table-responsive-lg">
+            <thead class="thead-light fl-bg-apricot fl-txt-prune fl-txt-hov-sadsea">
+            <tr>
+
+                <th class="medium"><a  href='ubbc-results2024.php?race=<?php echo $race ?>&order=raceid&asc=<?php echo $asc ?>'>Course</a></th>
+                <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=rrank&asc=<?php echo $asc ?>'>Rang Course</a></th>
+                <th class="medium"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=chrono&asc=<?php echo $asc ?>'>Chrono</a></th>
+                <th class="medium"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=faster&asc=<?php echo $asc ?>'>Record</a></th>
+                <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=bib&asc=<?php echo $asc ?>'>BIB</a></th>
+                <th class="large"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=lastname&asc=<?php echo $asc ?>'>Nom</a></th>
+                <th class="large"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=firstname&asc=<?php echo $asc ?>'>Pr&eacute;nom</a></th>
+                <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=gender&asc=<?php echo $asc ?>'>Genre</a></th>
+                <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=category&asc=<?php echo $asc ?>'>Cat&eacute;gorie</a></th>
+                <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=crank&asc=<?php echo $asc ?>'>Rang Genre</a></th>
+                <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=laps&asc=<?php echo $asc ?>'>Tours</a></th>
+                <th class="medium"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=duration&asc=<?php echo $asc ?>'>Durée</a></th>
+                <th class="medium"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=average&asc=<?php echo $asc ?>'>Moyene</a></th>
+                <th class="thin"><a href='ubbc-results2024.php?race=<?php echo $race ?>&order=srank&asc=<?php echo $asc ?>'>UBBC index</a></th>
+
+            </tr>
+            </thead>
+            <?php
 
 
-$sqlquery = <<< EOT
+            $sqlquery = <<< EOT
 SELECT
 l.bib,l.lastname,l.firstname,l.gender,l.category,
-l.faster,l.duration,l.laps,
+l.best_lap as faster,l.race_duration as chrono ,l.laps,
+l.average_lap as average,l.run_duration as duration,
 l.race,
 l.rrank,
 l.crank,
-l.srank
+l.srank,
+l.ubbc_index
 FROM results_2024 l
 EOT;
 
-mysqli_query($link,"SET SQL_BIG_SELECTS=1");
-$sqlquery = $sqlquery." ORDER BY ".$order." ".$asc." ,srank asc LIMIT ".(($cpage-1)*$nbperpage).",$nbperpage ;";
-$results=mysqli_query($link,$sqlquery) ;
-if (mysqli_num_rows($results) == 0) {
-   echo '</table>';
-   echo "<p class='m-3'>résultats indisponibles</p>";
-   exit;
-}
-echo '            <tbody>';
+            mysqli_query($link,"SET SQL_BIG_SELECTS=1");
+            $sqlquery = $sqlquery." ORDER BY ".$order." ".$asc." ,srank asc LIMIT ".(($cpage-1)*$nbperpage).",$nbperpage ;";
+            $results=mysqli_query($link,$sqlquery) ;
+            if (mysqli_num_rows($results) == 0) {
+                echo '</table>';
+                echo "<p class='m-3'>résultats indisponibles</p>";
+                exit;
+            }
+            echo '            <tbody>';
             //On affiche les lignes du tableau une à une à l'aide d'une boucle
 
             while($record = mysqli_fetch_array($results,MYSQLI_ASSOC))
             {
-                if ($record["ai"]==1){
-                  $aiico="<i class='ml-1' style='background-image:url(\"http://ubbc.fr/static/images/ico-synapse.png\");background-size: contain;background-repeat: no-repeat;width:15px;height:15px;display:inline-block;'></i>";
-                }
-                else {
-                  $aiico="";
-                }
 
-                if ($record["laps"]>=$record["goal"]){
-                  $expected="finisher";
-                  $expcolor="fl-txt-electric";
-                }
-                elseif ($record["expected"]=='23:59:59' || $record["control"]=='STOP'){
-                    $expected="stopped";
-                    $expcolor="fl-txt-peach";
-                  }
-                else {
-                  $expected=$record["expected"].$aiico;
-                  $expcolor="";
-                  if ($record["expected"][0]=='-') {
-                    $expcolor="fl-txt-peach";
-                  }
-                }
                 printf('<tr>');
-                printf('<td class="thin">%s</td>',$record["srank"]);
-                printf('<td class="thin %s">%s</td>',$lapscolor,$record["laps"]);
-                printf('<td class="thin"><a class="rounded border fl-txt-electric fl-bd-electric fl-bg-hov-white px-1 d-inline-block w-100 text-center" href="ubbc-userlaps.php?bib=%s">%s</a></td>',$record["bib"],$record["bib"]);
+                printf('<td class="medium text-capitalize">%s</td>',$record["race"]);
+                printf('<td class="thin text-uppercase">%s</td>',$record["rrank"]);
+                printf('<td class="medium">%s</td>',$record["chrono"]);
+                printf('<td class="medium">%s</td>',$record["faster"]);
+                printf('<td class="thin rounded border fl-txt-electric fl-bd-electric fl-bg-hov-white px-1 d-inline-block w-100 text-center">%s</td>',$record["bib"]);
                 printf('<td class="large text-capitalize">%s</td>',$record["lastname"]);
                 printf('<td class="large text-capitalize">%s</td>',$record["firstname"]);
                 printf('<td class="thin text-uppercase">%s</td>',$record["gender"]);
                 printf('<td class="thin text-uppercase">%s</td>',$record["category"]);
-                printf('<td class="medium">%s</td>',$record["faster"]);
-                printf('<td class="medium">%s</td>',$record["duration"]);
-                printf('<td class="medium text-capitalize">%s</td>',$record["race"]);
-                printf('<td class="thin text-uppercase">%s</td>',$record["rrank"]);
                 printf('<td class="thin text-uppercase">%s</td>',$record["crank"]);
+                printf('<td class="thin ">%s</td>',$record["laps"]);
+                printf('<td class="medium">%s</td>',$record["duration"]);
+                printf('<td class="medium">%s</td>',$record["average"]);
+                printf('<td class="thin">%s</td>',$record["ubbc_index"]);
                 printf('</tr>');
             } //fin de la boucle, le tableau contient toute la BDD
 
             mysqli_free_result($results);
             mysqli_close($link); //deconnection de mysql
-?>
+            ?>
             </tbody>
             <tfoot>
             </tfoot>
-            </table>
+        </table>
         <div class="m-auto">
-<?php
-    if ($nbpage>1) {
-      echo "<nav >";
-      echo "<ul class='pagination'>";
-    if ($cpage>1){
-         echo "<li class='page-item'><a class='page-link fl-txt-prune' href='ubbc-results2024.php?race=$race&page=".($cpage-1)."&order=$order&asc=$asc'>précédent</a></li>";
-    }
-    else {
-        echo "<li class='page-item disabled'><a class='page-link' href='#' tabindex='-1'>précédent</a></li>";
-    }
+            <?php
+            if ($nbpage>1) {
+                echo "<nav >";
+                echo "<ul class='pagination'>";
+                if ($cpage>1){
+                    echo "<li class='page-item'><a class='page-link fl-txt-prune' href='ubbc-results2024.php?race=$race&page=".($cpage-1)."&order=$order&asc=$asc'>précédent</a></li>";
+                }
+                else {
+                    echo "<li class='page-item disabled'><a class='page-link' href='#' tabindex='-1'>précédent</a></li>";
+                }
 
-    for($i=1;$i<=$nbpage;$i++) {
-        if ($i==$cpage){
-            echo "<li class='page-item disabled'><a class='page-link' href='#' tabindex='-1'>$i</a></li>";
-        }
-        elseif(abs($i-$cpage)<4) {
-            echo "<li class='page-item'><a class='page-link fl-txt-prune' href='ubbc-results2024.php?race=$race&page=$i&order=$order&asc=$asc'>$i</a></li>";
-        }
-    }
-    if ($cpage<$nbpage){
-         echo "<li class='page-item'><a class='page-link fl-txt-prune' href='ubbc-results2024.php?race=$race&page=".($cpage+1)."&order=$order&asc=$asc'>suivant</a></li>";
-    }
-    else {
-        echo "<li class='page-item disabled'><a class='page-link' href='#'>suivant</a></a>";
-    }
-    }
-?>
+                for($i=1;$i<=$nbpage;$i++) {
+                    if ($i==$cpage){
+                        echo "<li class='page-item disabled'><a class='page-link' href='#' tabindex='-1'>$i</a></li>";
+                    }
+                    elseif(abs($i-$cpage)<4) {
+                        echo "<li class='page-item'><a class='page-link fl-txt-prune' href='ubbc-results2024.php?race=$race&page=$i&order=$order&asc=$asc'>$i</a></li>";
+                    }
+                }
+                if ($cpage<$nbpage){
+                    echo "<li class='page-item'><a class='page-link fl-txt-prune' href='ubbc-results2024.php?race=$race&page=".($cpage+1)."&order=$order&asc=$asc'>suivant</a></li>";
+                }
+                else {
+                    echo "<li class='page-item disabled'><a class='page-link' href='#'>suivant</a></a>";
+                }
+            }
+            ?>
+        </div>
+        <a class ="mx-auto mb-2 btn btn-lg fl-bg-prune fl-txt-white fl-bg-hov-sadsea" href="http://www.ubbc.fr/ubbc-content.php">accueil</a>
+</section>
+<!--</div>-->
 </div>
-            <a class ="mx-auto mb-2 btn btn-lg fl-bg-prune fl-txt-white fl-bg-hov-sadsea" href="http://www.ubbc.fr/ubbc-content.php">accueil</a>
-          </section>
-        <!--</div>-->
-    </div>
 <?php include("ubbc-footer.html"); ?>
